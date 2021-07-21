9b5e43fc5874352f1304b116fda29c19cf520110
Welson Carvalho <welsonfscarvalho@gmail.com>
2021-07-21 18:00:52 +0200

    :construction: improve reader and writer in master

--- batch-master/src/main/java/com/example/batchmaster/job/PersonJob.java
+++ batch-master/src/main/java/com/example/batchmaster/job/PersonJob.java
@@ -1,7 +1,5 @@
 package com.example.batchmaster.job;
 
-import com.example.batchmaster.step.SavePersonReader;
-import com.example.batchmaster.step.SavePersonWriter;
 import lombok.AllArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -11,11 +9,20 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.TaskletStep;
 import org.springframework.batch.integration.chunk.ChunkHandler;
+import org.springframework.batch.integration.chunk.ChunkMessageChannelItemWriter;
 import org.springframework.batch.integration.chunk.ChunkResponse;
 import org.springframework.batch.integration.chunk.RemoteChunkHandlerFactoryBean;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.integration.core.MessagingTemplate;
+import org.springframework.messaging.PollableChannel;
 
 @Configuration
 @AllArgsConstructor
@@ -37,8 +44,8 @@ public class PersonJob {
 
     @Bean
     public TaskletStep stepSavePerson(
-            SavePersonReader savePersonReader,
-            SavePersonWriter savePersonWriter) {
+            ItemReader<String> savePersonReader,
+            ItemWriter<ChunkResponse> savePersonWriter) {
 
         return stepBuilderFactory
                 .get("step_save_person")
@@ -50,9 +57,33 @@ public class PersonJob {
 
     @Bean
     @StepScope
+    public FlatFileItemReader<String> savePersonReader() throws Exception {
+        return new FlatFileItemReaderBuilder<String>()
+                .name("savePersonReader")
+                .resource(new ClassPathResource("csv/person.csv"))
+                .lineTokenizer(new DelimitedLineTokenizer("\n"))
+                .fieldSetMapper(fieldSet -> fieldSet.readRawString(0)) // I want to get the line
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ChunkMessageChannelItemWriter<ChunkResponse> savePersonWriter(
+            MessagingTemplate messagingTemplate,
+            PollableChannel pollableChannel) {
+
+        ChunkMessageChannelItemWriter<ChunkResponse> writer = new ChunkMessageChannelItemWriter<>();
+        writer.setMessagingOperations(messagingTemplate);
+        writer.setReplyChannel(pollableChannel);
+
+        return writer;
+    }
+
+    @Bean
+    @StepScope
     public ChunkHandler<ChunkResponse> stepSavePersonHandler(
             TaskletStep stepSavePerson,
-            SavePersonWriter savePersonWriter) throws Exception {
+            ItemWriter<ChunkResponse> savePersonWriter) throws Exception {
 
         RemoteChunkHandlerFactoryBean<ChunkResponse> factoryBean = new RemoteChunkHandlerFactoryBean<>();
         factoryBean.setStep(stepSavePerson);
--- batch-slave/src/main/java/com/example/batchslave/step/SavePersonProcessor.java
+++ batch-slave/src/main/java/com/example/batchslave/step/SavePersonProcessor.java
@@ -4,9 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
+import java.time.format.DateTimeFormatter;
+
 @Slf4j
 @Component
 public class SavePersonProcessor implements ItemProcessor<Object, Object> {
+
+    private final DateTimeFormatter formatter = DateTimeFormatter
+            .ofPattern("[dd/MM/yyyy][dd/M/yyyy][d/M/yyyy]");
 
     @Override
     public Object process(Object o) throws Exception {
