6236ed30198715bfc5643977d2fbe43481fb19b4
Welson Carvalho <welsonfscarvalho@gmail.com>
2021-08-09 17:02:43 +0200

    :hammer: comment tests

--- batch-master/src/test/java/com/example/batchmaster/BatchMasterApplicationTests.java
+++ batch-master/src/test/java/com/example/batchmaster/BatchMasterApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.batchmaster;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BatchMasterApplicationTests {
 
 	@Test
